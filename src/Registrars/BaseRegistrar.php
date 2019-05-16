463c43297cd24586f93d66ca942dcf62dc9c5b50
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-05-16 04:09:20 +0200

    wip: remove old rest routes declarations

--- src/Registrars/BaseRegistrar.php
+++ src/Registrars/BaseRegistrar.php
@@ -136,45 +136,12 @@ abstract class BaseRegistrar
     /**
      * Register REST-style routes.
      *
-     * @param string $base The base URL.
-     * @param string $controller The controller
      * @param array $declarations The middleware
      */
-    protected function rest(string $base, string $controller, array $declarations = []): void
+    protected function rest(array $declarations = []): void
     {
-        $singular = Str::singular($base);
-
         foreach ($declarations as $key => $middleware) {
-            switch ($key) {
-                case "index":
-                    $this->get("$base/{page:\d+}/{perPage:[1-9]\d*}", [
-                        "uses" => "$controller@index",
-                        "middleware" => $middleware ?? null
-                    ]);
-                    break;
-                case "post":
-                    $this->post("$base", [
-                        "uses" => "$controller@post",
-                        "middleware" => $middleware ?? null
-                    ]);
-                    break;
-                case "get":
-                case "patch":
-                case "delete":
-                    $this->{$key}("$base/{{$singular}Id:[1-9]\d*}", [
-                        "uses" => "$controller@$key",
-                        "middleware" => $middleware ?? null
-                    ]);
-                    break;
-                default:
-                    if (Str::start($key, "related:")) {
-                        $relation = str_replace("related:", "", $key);
-                        $this->get("$base/{{$singular}Id:[1-9]\d*}/$relation/{page:\d+}/{perPage:[1-9]\d*}", [
-                            "uses" => "$controller@$relation",
-                            "middleware" => $middleware ?? null
-                        ]);
-                    }
-            }
+            $this->makeRESTRoute($key, $middleware);
         }
     }
 }
