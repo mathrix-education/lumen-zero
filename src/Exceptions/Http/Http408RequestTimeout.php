2aac1872223f4becf6640a0c77760c1ac6fae36a
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-12-13 22:36:59 +0100

    fix: http exceptions

--- src/Exceptions/Http/Http408RequestTimeout.php
+++ src/Exceptions/Http/Http408RequestTimeout.php
@@ -1,3 +1,18 @@
 <?php
 
 declare(strict_types=1);
+
+namespace Mathrix\Lumen\Zero\Exceptions\Http;
+
+/**
+ * Send a HTTP/408 Request Timeout
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/408
+ *
+ * @codeCoverageIgnore
+ */
+abstract class Http408RequestTimeout extends Http
+{
+    /** THE HTTP error standard code */
+    protected const CODE = 408;
+}
