5bce4b4d06270beabce343c3c57ec19de1f54eb0
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-12-13 22:37:52 +0100

    feat: add route not found exception

--- resources/lang/fr/exceptions.php
+++ resources/lang/fr/exceptions.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 return [
     'class_not_found'            => "La classe :class n'a pas pu être trouvée",
     'relation_deletion_restrict' => 'Le model :key ne peut pas être supprimé car il est restreint par la relation :relation',
+    'route_not_found'            => "La route :route n'existe pas",
 ];
---
+++ src/Exceptions/RouteNotFound.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mathrix\Lumen\Zero\Exceptions;
+
+use Mathrix\Lumen\Zero\Exceptions\Http\Http404NotFound;
+use Throwable;
+
+/**
+ * Thrown when a route does not exist.
+ */
+class RouteNotFound extends Http404NotFound
+{
+    public function __construct(string $route, ?Throwable $previous = null)
+    {
+        $data = ['route' => $route];
+        parent::__construct($data, trans('zero.exceptions.route_not_found', $data), $previous);
+    }
+}
